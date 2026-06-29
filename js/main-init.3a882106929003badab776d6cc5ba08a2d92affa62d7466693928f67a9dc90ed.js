adf11363c96cd4dc2ead33c3835356bc1a99f1b1
xiaochai_123 <l2556843965@163.com>
2026-06-29 15:24:15 +0800

    update

---
+++ js/main-init.3a882106929003badab776d6cc5ba08a2d92affa62d7466693928f67a9dc90ed.js
@@ -0,0 +1,42 @@
+(() => {
+  // ns-hugo-params:C:\Users\xiaochai_123\Desktop\myblog\themes\hugo-theme-bootstrap\assets\js\local-storage\index.ts
+  var local_storage_default = { baseURL: "http://localhost:47342/" };
+
+  // ns-hugo-imp:C:\Users\xiaochai_123\Desktop\myblog\themes\hugo-theme-bootstrap\assets\js\local-storage\index.ts
+  var PathLocalStorage = class {
+    constructor(baseURL) {
+      this.baseURL = baseURL;
+      if (baseURL.substring(0, 2) === "//") {
+        baseURL = "http:" + baseURL;
+      }
+      let url;
+      try {
+        url = new URL(baseURL);
+      } catch (e) {
+        url = new URL(baseURL, location.protocol + "//" + location.host);
+      }
+      const pathname = url.pathname.replace(/^(\/+)/, "").replace(/(\/+)$/, "");
+      if (pathname !== "") {
+        this.prefix += pathname.replace("/", "-") + ":";
+      }
+    }
+    prefix = "hbs:";
+    getItem(key) {
+      return localStorage.getItem(this.prefix + key);
+    }
+    setItem(key, value) {
+      localStorage.setItem(this.prefix + key, value);
+    }
+    removeItem(key) {
+      localStorage.removeItem(this.prefix + key);
+    }
+  };
+  var local_storage_default2 = new PathLocalStorage(local_storage_default.baseURL);
+
+  // <stdin>
+  (() => {
+    if (local_storage_default2.getItem("sidebar-toggle") !== null && document.querySelector("main")?.getAttribute("data-kind") === "page") {
+      document.querySelector("main")?.classList.add("sidebar-none");
+    }
+  })();
+})();
